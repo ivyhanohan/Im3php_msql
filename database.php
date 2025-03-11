99bc0be38fa53dd531572f5d0c0f32102f1b2a99
ivyhanohan <ivyhanohan@gmail.com>
2025-03-11 17:32:48 +0800

    IM3

---
+++ database.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername = "localhost"; 
+$username = "root";       
+$password = "";           
+$dbname = "mysql_guest_db";  
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+function addGuest($conn, $first_name, $last_name, $email, $photo, $document) {
+    $stmt = $conn->prepare("INSERT INTO guest_info (first_name, last_name, email, photo, document) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $first_name, $last_name, $email, $photo, $document);
+    $stmt->execute();
+    $stmt->close();
+}
+
+?>
